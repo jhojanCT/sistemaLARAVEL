fc661578343447ee4c3c572e5ac43379a229cee7
Deanilim <chambideanira@gmail.com>
2024-12-17 09:01:39 -0400

    fdghj

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Inventarios</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet"> <!-- Enlace al CSS personalizado -->
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Inventario</a>
+    <!-- Barra de navegación -->
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand ms-3" href="#">Inventario</a>
         <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav ms-3">
                 <li class="nav-item"><a class="nav-link" href="{{ route('categorias.index') }}">Categorías</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('productos.index') }}">Productos</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('proveedores.index') }}">Proveedores</a></li>
@@ -24,6 +26,7 @@
         </div>
     </nav>
 
+    <!-- Contenido principal -->
     <div class="container mt-4">
         @yield('content') <!-- Aquí se carga el contenido de cada vista -->
     </div>
